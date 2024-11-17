d1008e6dca5bb57479034c9b6a17e38ab63a1bd7
MechamJonathan <jonsmec95@gmail.com>
2024-11-17 06:05:45 -0700

    added textnode class

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+from textnode import *
+
 def main():
     print("Hello, World!")
+    node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+    print(node)
+
+
+
+
 
 if __name__ == '__main__':
     main()
